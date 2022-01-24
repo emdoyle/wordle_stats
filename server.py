aa62e33e719fa4142acf825c9e4047ffab6b6df5
Evan Doyle <evanmdoyle@gmail.com>
2022-01-24 14:43:18 -0800

    Import listeners in server.py

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 from app import settings
 from app.apps import app
+from app.listeners import *
 
 if __name__ == "__main__":
     SocketModeHandler(app, app_token=settings.SLACK_APP_TOKEN).start()
