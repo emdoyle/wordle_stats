63ad0cd5c8f07e7b30d7213fad06b518435710eb
Evan Doyle <evanmdoyle@gmail.com>
2022-02-04 11:35:46 -0800

    Quick and dirty pagination on conversations list API call

--- util/channels.py
+++ util/channels.py
@@ -10,5 +10,12 @@ def get_member_channel_ids(team_id: str) -> List[str]:
         return [settings.TESTING_CHANNEL_ID]
     force_client_auth(app, team_id)
     response = app.client.conversations_list(team_id=team_id)
+    metadata = response["response_metadata"]
+    next_cursor = metadata.get("next_cursor")
     channels = response["channels"]
+    while next_cursor:
+        response = app.client.conversations_list(cursor=next_cursor, team_id=team_id)
+        metadata = response["response_metadata"]
+        next_cursor = metadata.get("next_cursor")
+        channels.extend(response["channels"])
     return [channel["id"] for channel in channels if channel.get("is_member", False)]
